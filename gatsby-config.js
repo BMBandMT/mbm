27be0f283ef39deefe12ca253dc65b73a1231f2c
Alexander Fountain <alexander@digett.com>
2021-02-14 08:37:57 -0600

    ua id

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        trackingId: "UA-189575614",
       },
     },
     `gatsby-plugin-styled-components`,
